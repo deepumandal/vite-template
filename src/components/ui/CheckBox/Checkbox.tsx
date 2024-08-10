c875713bfa162d8eb7f159f2e675bd8fa17bf776
DEEPAK KUMAR MANDAL <dev.workwithdeepak@gmail.com>
2024-08-10 08:08:34 +0530

    checkbox ui component

--- src/components/ui/CheckBox/Checkbox.tsx
+++ src/components/ui/CheckBox/Checkbox.tsx
@@ -9,16 +9,11 @@ const Checkbox = forwardRef<
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-text",
-      className
-    )}
+    className={cn("checkbox", className)}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
-      <Check className="h-4 w-4" />
+    <CheckboxPrimitive.Indicator className={cn("checkbox-indicator")}>
+      <Check className="box-size" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
