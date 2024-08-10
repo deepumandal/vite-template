16b1418ce811453564dfa8da70b41cb77a11d71a
DEEPAK KUMAR MANDAL <dev.workwithdeepak@gmail.com>
2024-08-10 08:04:29 +0530

    Button ui component

--- src/components/ui/Button/utils/ButtonVariants.ts
+++ src/components/ui/Button/utils/ButtonVariants.ts
@@ -1,31 +1,26 @@
 import { cva } from "class-variance-authority";
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default: "text-heading-dark bg-primary hover:bg-primary/80",
-        destructive: "bg-destructive text-heading-dark hover:bg-destructive/80",
-        outline:
-          "border border-input text-heading bg-transparent hover:bg-secondary",
-        secondary: "bg-secondary text-heading hover:bg-secondary/80",
-        ghost:
-          "bg-transparent text-heading hover:bg-secondary/55 dark:hover:bg-secondary-dark/55",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-5 py-2",
-        sm: "h-9 rounded-md px-2",
-        lg: "h-11 rounded-md px-7",
-        icon: "h-10 w-10",
-      },
+const buttonVariants = cva("base-button", {
+  variants: {
+    variant: {
+      default: "default-button",
+      destructive: "destructive-button",
+      outline: "outline-button",
+      secondary: "secondary-button",
+      ghost: "ghost-button",
+      link: "button-link",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "button-default-size",
+      sm: "button-small-size",
+      lg: "button-large-size",
+      icon: "button-icon",
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 export { buttonVariants };
