29a820d16fa3a2362aa140dd55b6d27a23892fc9
DEEPAK KUMAR MANDAL <dev.workwithdeepak@gmail.com>
2024-08-10 08:16:00 +0530

    confirmation dialog ui component

--- src/components/ui/ConfirmationDialog/ConfirmationDialog.tsx
+++ src/components/ui/ConfirmationDialog/ConfirmationDialog.tsx
@@ -22,36 +22,36 @@ const ConfirmationDialog = ({
   handleConfirm,
 }: ConfirmationDialogProps) => (
   <Model modal={model} open={open} onOpenChange={onOpenChange}>
-    <Model.Content className="!top-[30%]" role="alertdialog">
+    <Model.Content className="dialog-content" role="alertdialog">
       <Model.Header>
         <Model.Title asChild>
-          <Typography
-            asElement="h3"
-            role="heading"
-            className="!text-lg !font-semibold"
-          >
+          <Typography asElement="h3" role="heading" className="dialog-title">
             {title ?? "Delete Item"}
           </Typography>
         </Model.Title>
       </Model.Header>
       <Model.Description asChild>
-        <Typography asElement="p" className="!text-sm !mt-4">
+        <Typography asElement="p" className="dialog-description">
           {body ?? "Are you sure you want to delete this item?"}
         </Typography>
       </Model.Description>
 
-      <Model.Footer className="!mt-6">
+      <Model.Footer className="dialog-footer">
         <Model.Close asChild>
           <Button
             variant={"destructive"}
             onClick={handleConfirm}
-            className="!px-4"
+            className="dialog-delete"
           >
             Delete
           </Button>
         </Model.Close>
         <Model.Close asChild>
-          <Button variant="outline" onClick={handleCancel} className="px-4">
+          <Button
+            variant="outline"
+            onClick={handleCancel}
+            className="dialog-close"
+          >
             Cancel
           </Button>
         </Model.Close>
