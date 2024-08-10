f2e60c8774f8e758dfb03425e60bdb1faae915cf
DEEPAK KUMAR MANDAL <dev.workwithdeepak@gmail.com>
2024-08-10 07:22:52 +0530

    accordion complete

--- src/components/ui/Accordion/Accordion.tsx
+++ src/components/ui/Accordion/Accordion.tsx
@@ -1,67 +1,88 @@
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import {
+  AccordionMultipleProps as _AccordionMultipleProps,
+  AccordionSingleProps as _AccordionSingleProps,
+  Content,
+  Header,
+  Item,
+  Root,
+  Trigger,
+} from "@radix-ui/react-accordion";
 import { ChevronDown } from "lucide-react";
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  forwardRef,
+  ReactNode,
+} from "react";
 import { cn } from "@Utils/className";
 
-interface AccordionMultipleProps
-  extends AccordionPrimitive.AccordionMultipleProps {}
-interface AccordionSingleProps
-  extends AccordionPrimitive.AccordionSingleProps {}
+interface AccordionMultipleProps extends _AccordionMultipleProps {}
+interface AccordionSingleProps extends _AccordionSingleProps {}
 
 type AccordionProps = AccordionMultipleProps | AccordionSingleProps;
 
 const Accordion = ({ children, className, ...rest }: AccordionProps) => (
-  <AccordionPrimitive.Root {...rest} className={cn("w-full", className)}>
+  <Root {...rest} className={cn("accordion-root", className)}>
     {children}
-  </AccordionPrimitive.Root>
+  </Root>
 );
 
 const AccordionItem = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Item>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+  ElementRef<typeof Item>,
+  ComponentPropsWithoutRef<typeof Item>
 >(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item
-    ref={ref}
-    className={cn("border-b", className)}
-    {...props}
-  />
+  <Item ref={ref} className={className} {...props} />
 ));
 AccordionItem.displayName = "AccordionItem";
 
+interface AccordionTriggerProps
+  extends ComponentPropsWithoutRef<typeof Trigger> {
+  icon?: ReactNode;
+  rotate?: boolean | string;
+}
+
 const AccordionTrigger = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
-    <AccordionPrimitive.Trigger
+  ElementRef<typeof Trigger>,
+  AccordionTriggerProps
+>(({ className, children, icon, rotate = true, ...props }, ref) => (
+  <Header className="flex">
+    <Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 text-heading items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "accordion-trigger",
+        typeof rotate == "boolean"
+          ? rotate
+            ? "transition-all [&[data-state=open]>svg]:rotate-180"
+            : ""
+          : rotate,
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
-    </AccordionPrimitive.Trigger>
-  </AccordionPrimitive.Header>
+
+      {icon || (
+        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      )}
+    </Trigger>
+  </Header>
 ));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+AccordionTrigger.displayName = Trigger.displayName;
 
 const AccordionContent = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+  ElementRef<typeof Content>,
+  ComponentPropsWithoutRef<typeof Content>
 >(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Content
+  <Content
     ref={ref}
     className="overflow-hidden text-text text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
-  </AccordionPrimitive.Content>
+  </Content>
 ));
 
-AccordionContent.displayName = AccordionPrimitive.Content.displayName;
+AccordionContent.displayName = Content.displayName;
 
 Accordion.Item = AccordionItem;
 Accordion.Trigger = AccordionTrigger;
